beb0defc9f24f16fe5c22bd4d905e8a9adb6affb
yaohuang <yao.huang@live.com>
2013-09-25 00:02:22 -0700

    added readme.txt and install/uninstall scripts.

---
+++ WebApiTestClient/WebApiTestClient/readme.txt
@@ -0,0 +1,10 @@
+﻿-------------------
+Web API Test Client 
+-------------------
+A simple Test Client built on top of ASP.NET Web API Help Page.
+
+Upon installation, the package will automatically attempt to inject 
+the following test client code into Areas\HelpPage\Views\Help\Api.cshtml
+	@Html.DisplayForModel("TestClientDialogs")
+	@Html.DisplayForModel("TestClientReferences")
+That's it. No additional setup required when it's completed successfully.
---
+++ WebApiTestClient/WebApiTestClient/tools/Uninstall.ps1
@@ -0,0 +1,9 @@
+﻿param($installPath, $toolsPath, $package, $project)
+$file = $project.ProjectItems.Item("Areas").ProjectItems.Item("HelpPage").ProjectItems.Item("Views").ProjectItems.Item("Help").ProjectItems.Item("Api.cshtml")
+if($file) {
+    $file.Open()
+    $file.Document.Activate()
+    $file.Document.Selection.StartOfDocument()
+    $file.Document.ReplaceText("@Html.DisplayForModel(`"TestClientDialogs`")`n@section Scripts {`n    @Html.DisplayForModel(`"TestClientReferences`")", "@section Scripts {")
+	$file.Save()
+}
