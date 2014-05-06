0201d6e8a766d25da6f2f8908751f7320f4c41e2
Taha Azab <azab.taha@gmail.com>
2014-05-06 11:28:38 +0200

    Added Headers List to TestClientViewModel

--- WebApiTestClient/WebApiTestClient/Scripts/WebApiTestClient.js
+++ WebApiTestClient/WebApiTestClient/Scripts/WebApiTestClient.js
@@ -71,6 +71,39 @@ var emptyTestClientModel =
         }
 
         self.RequestHeaders = ko.observableArray();
+        
+        self.headersList = ["Accept",
+            "Accept-Charset",
+            "Accept-Encoding",
+            "Accept-Language",
+            "Accept-Datetime",
+            "Authorization",
+            "Cache-Control",
+            "Connection",
+            "Cookie",
+            "Content-Length",
+            "Content-MD5",
+            "Content-Type",
+            "Date",
+            "Expect",
+            "From",
+            "Host",
+            "If-Match",
+            "If-Modified-Since",
+            "If-None-Match",
+            "If-Range",
+            "If-Unmodified-Since",
+            "Max-Forwards",
+            "Origin",
+            "Pragma",
+            "Proxy-Authorization",
+            "Range",
+            "Referer",
+            "TE",
+            "User-Agent",
+            "Upgrade",
+            "Via",
+            "Warning"];
 
         self.RequestMediaType = ko.observable();
 
